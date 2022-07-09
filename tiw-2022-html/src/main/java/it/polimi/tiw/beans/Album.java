584e636c616c33688b91e3b01c6f6f0dc2c264fa
Matteo Spreafico <matteo2.spreafico@mail.polimi.it>
2022-07-09 11:24:08 +0200

    home page basics added

---
+++ tiw-2022-html/src/main/java/it/polimi/tiw/beans/Album.java
@@ -0,0 +1,35 @@
+package it.polimi.tiw.beans;
+
+import java.sql.Date;
+
+public class Album {
+	
+	private String owner;
+	private String title;
+	private Date creationDate;
+	
+	public String getOwner() {
+		return owner;
+	}
+	
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+}
