ec4059f3c8be2864d5a579456e43e34bf0649239
Matteo Spreafico <matteo2.spreafico@mail.polimi.it>
2022-07-12 16:00:26 +0200

    HomePage album visualization fixed

--- tiw-2022-html/src/main/java/it/polimi/tiw/DAO/AlbumDAO.java
+++ tiw-2022-html/src/main/java/it/polimi/tiw/DAO/AlbumDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import it.polimi.tiw.beans.Album;
@@ -18,13 +19,13 @@ public class AlbumDAO {
 	}
 
 	public List<Album> getUserAlbums(String username) throws SQLException {
-		String query = "SELECT * FROM album WHERE proprietario = ?";
+		String query = "SELECT * FROM album WHERE proprietario = ? ORDER BY data_creazione DESC";
 		List<Album> userAlbums = buildAlbumList(query, username);
 		return userAlbums;
 	}
 	
 	public List<Album> getOtherAlbums(String username) throws SQLException {
-		String query = "SELECT * FROM album WHERE proprietario != ?";
+		String query = "SELECT * FROM album WHERE proprietario <> ? ORDER BY data_creazione DESC";
 		List<Album> otherAlbums = buildAlbumList(query, username);
 		return otherAlbums;
 	}
@@ -34,7 +35,7 @@ public class AlbumDAO {
 			pstatement.setString(1, username);
 			try (ResultSet result = pstatement.executeQuery();) {
 				if (!result.isBeforeFirst()) // no results
-					return null;
+					return Collections.emptyList();
 				else {
 					List<Album> albums = new ArrayList<>();
 					while(result.next()) {
--- tiw-2022-html/src/main/java/it/polimi/tiw/servlets/HomePage.java
+++ tiw-2022-html/src/main/java/it/polimi/tiw/servlets/HomePage.java
@@ -2,6 +2,9 @@ package it.polimi.tiw.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -16,6 +20,8 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import it.polimi.tiw.DAO.AlbumDAO;
+import it.polimi.tiw.beans.Album;
+import it.polimi.tiw.beans.User;
 import it.polimi.tiw.utils.ConnectionHandler;
 
 /**
@@ -54,13 +60,25 @@ public class HomePage extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("utente");
 		AlbumDAO albumDAO = new AlbumDAO(connection);
-		// TODO
+		List<Album> userAlbums = new ArrayList<>();
+		List<Album> otherAlbums = new ArrayList<>();
+		try {
+			userAlbums.addAll(albumDAO.getUserAlbums(user.getUsername()));
+			otherAlbums.addAll(albumDAO.getOtherAlbums(user.getUsername()));
+		} 
+		catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Impossibile caricare gli album.");
+			return;
+		}
 		
-		// Redirect to the Home page
 		String path = "/WEB-INF/templates/HomePage.html";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		ctx.setVariable("userAlbums", userAlbums);
+		ctx.setVariable("otherAlbums", otherAlbums);
 		templateEngine.process(path, ctx, response.getWriter());
 	}
 
